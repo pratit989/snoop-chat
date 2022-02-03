b0a7c3633c43618cabd9f68f6d11b0b017a879b8
Pratit Shivaji Todkar <pratittodkar@gmail.com>
2022-02-04 02:36:45 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import HTMLFlipBook from 'react-pageflip';
+import React from 'react';
+import useWindowDimensions from './windowDimensions';
+
+function importAll(r) {
+  let images = {};
+  r.keys().map((item, index) => { images[item.replace('./', '')] = r(item); });
+  return images;
+}
+
+const images = importAll(require.context('../public/pages', false, /\.(png|jpe?g|svg)$/));
+const values = Object.keys(images).map(function(k){return images[k]});
+
+const PageCover = React.forwardRef((props, ref) => {
+  return (
+    <div className="page page-cover" ref={ref} data-density="hard">
+      <img src={values[props.number]} style={{height: "inherit", width: "inherit"}}/>
+    </div>
+  );
+});
+
+const Page = React.forwardRef((props, ref) => {
+  return (
+      <div className="demoPage" ref={ref} data-density="soft">
+          <img src={values[props.number]} style={{height: "inherit", width: "inherit"}}/>
+      </div>
+  );
+});
+
+var pagesList = [];
+for (var i = 1; i < values.length-1; i++) {
+    // note: we are adding a key prop here to allow react to uniquely identify each
+    // element in this array. see: https://reactjs.org/docs/lists-and-keys.html
+    pagesList.push(<Page number={i} key={i}/>);
+}
+console.log(pagesList);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const { height, width } = useWindowDimensions();
+    return (
+      <div>
+        <HTMLFlipBook width={height/1.58} height={height}>
+            <PageCover number="0">Human Amelioration Vol. 2</PageCover>
+            {pagesList}
+            <PageCover number="47">The End</PageCover>
+        </HTMLFlipBook>
+      </div>
   );
 }
 
