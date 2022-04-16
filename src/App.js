1cefc7deca371e2ada11ebf396fb6f79f8b17b4f
Pratit Shivaji Todkar <Pratittodkar@gmail.com>
2022-04-16 20:49:36 +0530

    Uploaded All the Pages

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function importAll(r) {
   return images;
 }
 
-const images = Object.assign(importAll(require.context('../public/vol1', false, /\.(png|jpe?g|svg)$/)), importAll(require.context('../public/vol2', false, /\.(png|jpe?g|svg)$/)));
+const images = Object.assign(importAll(require.context('../public/vol1', false, /\.(png|jpe?g|svg)$/)));
 const values = Object.keys(images).map(function(k){return images[k]});
 
 const PageCover = React.forwardRef((props, ref) => {
@@ -41,23 +41,11 @@ function App() {
   const [magazine, setMagazine] = useState('');
     return (
       <div>
-        <div className="page page-cover" data-density="hard" style={{width: "initial", display: `${magazine===''?'flex':'none'}`, height: "100vh", justifyContent: "center", alignItems: "center"}}>
-          <button class="first" onClick={()=> setMagazine(
-          <HTMLFlipBook width={width > height/1.58 ? height/1.58 : width} height={height}>
-            <PageCover number="0">Human Amelioration Vol. 1</PageCover>
+        <HTMLFlipBook width={width > height/1.58 ? height/1.58 : width} height={height}>
+            <PageCover number="0">SnoopChat</PageCover>
             {pagesList.splice(0,49)}
-            <PageCover number="50">The End</PageCover>
+            <PageCover number="29">The End</PageCover>
           </HTMLFlipBook>
-          )}>Volume 1</button>
-          <button class="sec" onClick={()=> setMagazine(
-          <HTMLFlipBook width={width > height/1.58 ? height/1.58 : width} height={height}>
-            <PageCover number="51">Human Amelioration Vol. 1</PageCover>
-            {pagesList.splice(51)}
-            <PageCover number="98">The End</PageCover>
-          </HTMLFlipBook>
-          )}>Volume 2</button>
-        </div>
-        {magazine}
       </div>
   );
 }
